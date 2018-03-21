24c7cf0c5d2c03e2882b8b588722a57f70d96e3a
dalbela-gt <david.albela@es.gt.com>
2018-03-21 10:24:40 +0100

    Add sign transaction method

--- index.js
+++ index.js
@@ -1,13 +1,44 @@
-var Web3 = require('web3');
-var web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));//with testrpc
-web3.eth.defaultAccount = web3.eth.accounts[0];
+const EthereumTx = require('ethereumjs-tx')
+const Web3 = require('web3');
+var SolidityFunction = require('web3/lib/web3/function');
+var web3; //with testrpc
 
 exports.setProvider = function (web3Instance){
   web3 = web3Instance;
 }
 
+exports.encodeFunctionCall = function (abi, functionName, functionParams, contractAddress) {
+  var nameMethod = '[encodeFunctionCall]';
+  console.log("%s %s Encoding call function %s -> with params -> ", nameModule, nameMethod, JSON.stringify(functionName), JSON.stringify(functionParams));
+  var solidityFunction = new SolidityFunction('', _.find(abi, {name: functionName}), contractAddress);
+  return solidityFunction.toPayload(functionParams).data;
+}
+
+exports.signTransaction = function (nonce, gasPrice, gasLimit, to, value, data, chainId, privateKey) {
+  var nameMethod = '[signTransaction]';
+  console.log("%s %s Signing transaction", nameModule, nameMethod);
+  var tx = new EthereumTx(null, chainId);
+  tx.nonce = web3.toHex(nonce);
+  tx.gasPrice = web3.toHex(gasPrice);
+  tx.gasLimit = web3.toHex(gasLimit);
+  tx.value = web3.toHex(value);
+  // for creating new contract, parameter to is Nil
+  if (to != null){
+    tx.to = to;
+  }
+  tx.data = data;
+  var privateKeyBuffer = new Buffer(privateKey, 'hex');
+  tx.sign(privateKeyBuffer);
+  console.log("%s %s Signed transaction", nameModule, nameMethod);
+  return '0x' + tx.serialize().toString('hex');
+}
+
 exports.setOtherAccount = function (){
   return web3.eth.accounts[1];
+}
+
+exports.setAccount = function (account){
+  web3.eth.defaultAccount = account;
 }
 
 exports.setDefaultAccount = function (account){
